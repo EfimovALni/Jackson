3d4d5002401f8618809fec8ca47433fa9a0e5949
Alexander <EfimovAlNi@ya.ru>
2018-10-09 12:47:51 +0200

    Десериализем эту строку json_1 = "{\"status\":\"OK\",\"actual_version\":0.01}";

--- app/src/main/java/cz/firstapp/jackson_3/MainActivity.java
+++ app/src/main/java/cz/firstapp/jackson_3/MainActivity.java
@@ -1,14 +1,13 @@
 package cz.firstapp.jackson_3;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,51 +16,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        String jsonResponce = "{\"status\":\"OK\",\"actual_version\":0.01,\"configuartion\":{\"initial_screen\":[{\"color\":\"#0000ff\",\"text\":\"software\",\"icon\":\"data:image/png;base64,iVB=\"},{\"color\":\"#0000ff\",\"text\":\"citrix\",\"icon\":\"data:image/png;base64,iVBO+A=\"},{\"color\":\"#0000ff\",\"text\":\"equipment\",\"icon\":\"data:image/png;base64,iVB=\"}]}}";
 
-        MainActivity mainActivity = new MainActivity();
+        Configuration config = new Configuration();
+        ObjectMapper mapper = new ObjectMapper();
+        String json_1 = "{\"status\":\"OK\",\"actual_version\":0.01}";
+//        String json_2 = "{\"status\":\"OK\",\"actual_version\":0.01,\"configuartion\":{\"initial_screen\":[]}}";
+
 
         try {
-            ObjectMapper mapper = new ObjectMapper();
-
-            Map<String, Object> studentDataMap = new HashMap<String, Object>();
-
-            int[] marks = {4,5,3};
-
-            Student student = new Student();
-            student.setAge(25);
-            student.setName("Michail");
-
-//            Java Object
-            studentDataMap.put("student", student);
-//            Java String
-            studentDataMap.put("name", "Michael Kamar");
-//            Java Boolean
-            studentDataMap.put("verified", Boolean.FALSE);
-//            Array
-            studentDataMap.put("marks", marks);
-
-//            Serialize
-            String jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(studentDataMap);
-            Log.e("^^^", jsonString);
-            /* Answer:
-            {
-                "name" : "Michael Kamar",
-                    "student" : {
-                "age" : 25,
-                        "name" : "Michail"
-            },
-                "marks" : [ 4, 5, 3 ],
-                "verified" : false
-            }*/
-
-//            DeSerialize
-            studentDataMap = mapper.readValue(jsonString, Map.class);
-            Log.e("-------- ", String.valueOf(studentDataMap.get("name")));
-            /* Answer
-            Michael Kamar*/
+//            Deserialize
+            Configuration configuration = mapper.readValue(json_1, Configuration.class);
+            Log.e("json_1 (DeSer)", configuration.toString());
+            Log.e("json_1. Status ", configuration.getStatus() + ". Actual version: " + configuration.getActual_version());
 
 
-        } catch (Exception e) {
+
+
+
+
+
+////            Serialize
+//            json_1 = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(config);
+//            Log.e("json_1 (Ser)", json_1);
+
+
+
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
@@ -72,32 +54,34 @@ public class MainActivity extends AppCompatActivity {
 }
 
 
-class Student {
-    private int age;
-    private String name;
-    public Student(){}
+class Configuration {
+    private String status;
+    private double actual_version;
 
-    public int getAge() {
-        return age;
+    public String getStatus() {
+        return status;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setStatus(String status) {
+        this.status = status;
     }
 
-    public String getName() {
-        return name;
+    public double getActual_version() {
+        return actual_version;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setActual_version(double actual_version) {
+        this.actual_version = actual_version;
     }
 
+    @NonNull
     @Override
     public String toString() {
-        return "Student{" +
-                "age=" + age +
-                ", name='" + name + '\'' +
+        return "Configuration{" +
+                "status='" + status + '\'' +
+                ", actual_version=" + actual_version +
                 '}';
     }
 }
+
+
